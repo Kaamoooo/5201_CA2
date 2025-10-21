a6e72be9176482bb6a493ac9656c57d963fd8bd7
Yang Chen <youngchen64@foxmail.com>
2025-10-21 15:21:44 +0800

    Removed numpy

--- include.py
+++ include.py
@@ -1,4 +1,4 @@
-﻿import numpy as np
+﻿import math
 
 problem_sizes = [10000,50000,100000,500000,1000000,5000000,10000000,20000000]
 processor_nums = [1,2,4,8]
@@ -7,4 +7,4 @@ a = 0
 b = 5
 
 def target_function(x):
-    return np.exp(-x) * np.sin(5 * x)
+    return math.exp(-x) * math.sin(5 * x)
